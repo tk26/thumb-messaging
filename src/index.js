10ecb9ca9a1fd9c742b8800ff033780197b1a322
jdromine <jromines@gmail.com>
2018-09-19 07:11:06 -0400

    THUM-241 - Adding additional library exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const MessagingService = require('./src/services/MessagingService');
+const MessageDeliveryService = require('./services/MessageDeliveryService');
+const Message = require('./src/domain/Message');
+const MessageTypes = require('./domain/MessageTypes');
 
 module.exports = {
-  MessagingService
+  MessagingService,
+  MessageDeliveryService,
+  Message,
+  MessageTypes
 }
